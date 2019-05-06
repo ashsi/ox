e656acca10763933b2c414963587a5b7b141bb67
Ashley Singh <34393084+ashsi@users.noreply.github.com>
2019-05-06 16:56:51 +0100

    Update ps1.scala

--- ip2/ps1.scala
+++ ip2/ps1.scala
@@ -84,4 +84,59 @@ class LoggedEllipse(private var _a: Int, private var _b: Int) extends Ellipse(_a
 
 //[Q6]
 
-// Each line could be an different object with a different instance of Text containing the text of that line.
+/*(a) Each line could be a different instance of Text containing the text of that line.
+
+ (b) use the iterator trait to iterate through the instances of Text
+trait Iterator[T] {
+  def hasNext(): Bool
+  def next(): T
+}
+
+ (c) This change makes the fragile base class problem better because there is no longer a subclass relationship
+ between PlainText and Text, so their method use does not overlap and encapsulation is not broken */
+
+//[Q7]
+
+
+class Rectangle1(var width:Int, var height:Int) {
+	def ==(other: Rectangle1): Boolean =
+		this.width == other.width && this.height == other.height
+}
+
+// Tests for equality method:
+
+object Rectangle1 {
+	def main(args:Array[String]){
+		val r1 = new Rectangle1(20,30)
+		val r2 = new Rectangle1(20,30)
+		println(r1.hashCode == r2.hashCode) // Incorrectly prints false
+	}
+}
+
+class Rectangle2(var width:Int, var height:Int) {
+	override def equals(other: Any): Boolean =
+  		other match {
+  			case other : Rectangle2 => this.width == other.width && this.height == other.height && this.hashCode == other.hashCode
+  			case _ => false
+  		}
+  	override def hashCode : Int = width * 31 + height
+}
+
+// To get correct == result, overrided the hashCode function
+// To use in scala collections, changed equals to take Any type.
+
+// Tests
+
+object Rectangle2 {
+	def main(args:Array[String]){
+		val r1 = new Rectangle2(20,30)
+		val r2 = new Rectangle2(20,30)
+		val r3 = new Rectangle2(18,92)
+		//All tests should run true. And they do.
+		//r3 has the same hashCode as r1 and r2.
+		println(r1.hashCode == r2.hashCode && r2.hashCode == r3.hashCode)
+		println(r1 == r2)
+		println(r2 != r3)
+		println(r1 != r3)
+	}
+}
